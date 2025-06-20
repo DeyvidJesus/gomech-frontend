fc943120d88ce0ea11957989d751b4d49dc3c02b
DeyvidJesus <deyvid.gondim@econverse.com.br>
2025-06-20 03:19:57 -0300

    feat: fixing styles

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,18 +1,16 @@
 import Image from 'next/image';
-import { Aside, Logo, Nav, MenuItem } from './styles';
+import { Aside, Nav, MenuItem, MenuItemLink } from './styles';
 
 export default function Sidebar() {
   return (
     <Aside>
-      <Logo>
-        <Image src="/logo.png" alt="GoMech" width={150} height={150} />
-      </Logo>
+      <Image src="/logo.svg" alt="GoMech" width={80} height={80} />
       <Nav>
-        <MenuItem href="/">Dashboard</MenuItem>
-        <MenuItem href="/clientes">Clientes</MenuItem>
-        <MenuItem href="/veiculos">Veículos</MenuItem>
-        <MenuItem href="/os">Ordens de Serviço</MenuItem>
-        <MenuItem href="/configuracoes">Configurações</MenuItem>
+        <MenuItem><MenuItemLink href="/">Dashboard</MenuItemLink></MenuItem>
+        <MenuItem><MenuItemLink href="/clientes">Clientes</MenuItemLink></MenuItem>
+        <MenuItem><MenuItemLink href="/veiculos">Veículos</MenuItemLink></MenuItem>
+        <MenuItem><MenuItemLink href="/os">Ordens de Serviço</MenuItemLink></MenuItem>
+        <MenuItem><MenuItemLink href="/configuracoes">Configurações</MenuItemLink></MenuItem>
       </Nav>
     </Aside>
   );
