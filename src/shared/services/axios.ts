1b17be7e4abb2dee4939cc5af42e3bd383ef9ba6
DeyvidJesus <deyvid.gondim@econverse.com.br>
2025-09-13 02:56:46 -0300

    fix: using production links

--- src/shared/services/axios.ts
+++ src/shared/services/axios.ts
@@ -2,8 +2,8 @@ import { redirect } from "@tanstack/react-router";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5080",
-  // baseURL: "https://clear-ellene-deyvidgondim-8b8a208e.koyeb.app",
+  // baseURL: "http://localhost:5080",
+  baseURL: "https://clear-ellene-deyvidgondim-8b8a208e.koyeb.app",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
