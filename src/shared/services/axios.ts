b787cbc1323c61851d67f4b7c65cc73b90193ded
Deyvid Gondim <deyvid.gondim@econverse.com.br>
2025-10-29 18:54:54 -0300

    Update axios baseURL to new API endpoint

--- src/shared/services/axios.ts
+++ src/shared/services/axios.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const api = axios.create({
   // baseURL: "http://localhost:5080",
-  baseURL: "https://clear-ellene-deyvidgondim-8b8a208e.koyeb.app",
+  baseURL: "https://api.gomech.com.br",
   timeout: 30000,
   headers: {
     "Content-Type": "application/json",
